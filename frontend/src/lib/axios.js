dea10a1dcecaacdce0ad381f2be9a3eaa6352d27
Shruti Jain <shrutijain1221@gmail.com>
2026-04-03 14:09:26 +0530

    fix: api base url for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5000/api/notes":"/api"
+const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5000/api/notes":"/api/notes"
 const api=axios.create({
     baseURL:BASE_URL
 })
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -24,15 +24,13 @@ fetchNotes()
    <>
    <div className='min-h-screen'>
      <Navbar/>
-<div className='mt-5 p-4 mx-auto'>
-  {notes.length===0 && (
-    <NoNotesFound/>
-  )}
-<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 '>
-{notes.length>0 && notes.map((item)=>(
-  <NotesCard key={item?._id} note={item} setNotes={setNotes}/>
-))}
-</div>
+{Array.isArray(notes) && notes.length === 0 && <NoNotesFound />}
+
+<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 px-4 py-2'>
+  {Array.isArray(notes) &&
+    notes.map((item) => (
+      <NotesCard key={item?._id} note={item} setNotes={setNotes} />
+    ))}
 </div>
    </div>
    </>
